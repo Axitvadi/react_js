742c40b9fcea88d1b90fd99da4db1b09699eae5c
Axitvadi <axitvadi@gmail.com>
2022-07-17 09:56:54 +0530

    feat : convert to uppercase functionalities added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@
 // import './App.css';
 
 import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
     <Navbar title="Shiv" contact="Contact Us" />
+    <div className="container">
+    <TextForm heading="Enter the text to analyze"/>
+    </div>
     </>
   );
 }
